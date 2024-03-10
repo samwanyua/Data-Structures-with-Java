b01b6b234fe7a439f6ebe7795212c8a0f61116dd
samwanyua <samexample8@gmail.com>
2024-03-10 19:46:20 +0300

    Introduction to graphs

---
+++ DSA/Graphs.java
@@ -0,0 +1,20 @@
+public class Graphs {
+    /*
+     * Graphs - non-linear aggregation of nodes and edges
+     * Nodes(vertex) - contains data
+     * Edges - connection between two nodes
+     * Graphs - directed and undirected graphs
+     * Undirected graphs - social network like facebook
+     * When two nodes are connected - adjacency
+     * Directed graphs - one way connection
+     * ex. street maps
+     * Rep. a graph : adjacency matrix, adjacency list
+     * Adjacency matrix:
+        * time complexity O(1)
+        * space complexity O(n^2)
+        Adjacency list
+            * is an arraylist of linked lists
+            * time complexity - O(n)
+            * space complexity O(V + E)
+     */
+}
