3d3b1d290d77dc318084ec5116ab4508970580cc
samwanyua <samexample8@gmail.com>
2024-03-11 11:24:12 +0300

    Introduction to trees

---
+++ DSA/Trees.java
@@ -0,0 +1,29 @@
+public class Trees {
+    /*
+     * Trees
+     * Is a non-linear data structure where nodes are organized in a hierachy
+     * Has nodes and edges
+     * Ex. of a tree - family tree
+     * Application of trees:
+     *      - File explorer
+     *      - Databases
+     *      - DNS (Domain Name Server)
+     *      - Html DOM
+     * Root node/parent - top of the tree, had no incoming edges but only outgoing edges
+     * Leaf nodes/child - at the bottom of the tree, have no outgoing edges but have incoming edges
+     * Branch n0des - have incoming and outgoing edges
+     * Siblings - share the same parent
+     * Subtree - is a smaller tree held within a larger tree
+     * 
+     * Size of a tree = # of nodes
+     * Depth of a tree = # of edges below root node
+     * Height of a tree = # of edges above the furthest leaf node
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+}
